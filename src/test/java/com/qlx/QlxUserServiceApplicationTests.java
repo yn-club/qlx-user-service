83ab884997fa52ff6767482ab6eccec38414b38e
yangning <yangning110302@163.com>
2020-04-11 20:06:10 +0800

    add

--- src/test/java/com/qlx/QlxUserServiceApplicationTests.java
+++ src/test/java/com/qlx/QlxUserServiceApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class QlxUserServiceApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
 		System.out.println("============");
